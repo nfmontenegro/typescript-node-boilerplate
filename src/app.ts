683d1806ca6ca46f470c55e7ed13ad76fb8e6bbf
nfmontenegro <nicolas.fmontenegro@gmail.com>
2019-03-24 21:00:03 -0300

    first config

--- src/app.ts
+++
@@ -1,8 +0,0 @@
-
-import express, {Application} from 'express'
-
-const app: Application = express();
-
-app.listen(3000, () => {
-  console.info('Server running in port 3000')
-})
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import express, {Request, Response} from 'express'
+const app = express()
+
+app.get('/', (req: Request, res: Response): any => res.send('Hello World!'))
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
