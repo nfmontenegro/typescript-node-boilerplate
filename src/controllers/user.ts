ff4bea32faba3059555a6946d497f4fd548e572d
Nicolás <nicolas.fmontenegro@gmail.com>
2019-05-14 16:53:36 -0400

    remove unnecesary types validations

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -8,15 +8,15 @@ import {IUser} from '../interfaces/user'
 export const createUser = async (req: Request, res: Response): Promise<Response | void> => {
   try {
     if (!Object.keys(req.body).length) res.status(200).json({message: 'Empty body!'})
-    const SALT_ROUND: number = 10
-    const {password, email}: {password: string; email: string} = req.body
+    const SALT_ROUND = 10
+    const {password, email} = req.body
     const userRepository = getManager().getRepository(User)
 
     const user: IUser = await userRepository.findOne({where: {email}})
     if (user) res.status(200).json({message: `User with email ${email} exist!`})
 
-    const hashedPassword: string = await bcrypt.hash(password, SALT_ROUND)
-    const userCreated: object = userRepository.create({...req.body, password: hashedPassword})
+    const hashedPassword = await bcrypt.hash(password, SALT_ROUND)
+    const userCreated = userRepository.create({...req.body, password: hashedPassword})
     const userSaved: IUser = await userRepository.save(userCreated)
 
     res.json(userSaved)
@@ -26,7 +26,7 @@ export const createUser = async (req: Request, res: Response): Promise<Response 
 }
 
 export const getUser = async (req: Request, res: Response): Promise<Response | void> => {
-  const id: number = req.params.id
+  const id = req.params.id
   const userRepository = getManager().getRepository(User)
   const user: IUser = await userRepository.findOne(id)
 
@@ -37,7 +37,7 @@ export const getUser = async (req: Request, res: Response): Promise<Response | v
 
 export const getUsers = async (req: Request, res: Response): Promise<Response | void> => {
   const userRepository = getManager().getRepository(User)
-  const users: Array<object> = await userRepository.find()
+  const users = await userRepository.find()
 
   res.status(200).json(users)
 }
